bc63e7e0384d9e22432545dd84b29af91b26ca0f
Steve Lacy <me@slacy.me>
2014-02-28 18:00:52 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-Spritzify
-=========
+# Spritzify
 
-jQuery rebuild of OpenSpritz
+##### jQuery rebuild of OpenSpritz
+
+
+
+### Example
+
+```js
+
+// Spritzify a html input containing text
+$('#textblock').spritzify();
+
+
+// Set a word per minute speed
+$('#textblock').spritzify({wpm: '250'})
+
+```
+
+
+## API
+
+#### options.wpm
+    
+    type: `Number`
+    default: `500`
+
+example:
+
+```js
+
+var options = {
+  wpm: 250
+}
+
+$('#textblock').spritzify(options)
+
+```
---
+++ js/spritz.js
@@ -0,0 +1,53 @@
+$.fn.spritzify = function(opts){
+  if(!opts) opts = {};
+  if(!opts.wpm) opts.wpm = 500;
+  opts.wpm = 60000/opts.wpm;
+
+  var element = this.selector;
+  var text = this.text().split(' ');
+
+  $(element).addClass('spritzify');
+
+  var showWord = function(word, cb){
+    var delay = opts.wpm;
+    var length = word.length;
+
+    if(word.indexOf(',') === word.length-1) delay += (delay *1.3);
+    if(word.indexOf(';') === word.length-1 || word.indexOf('?') === word.length-1 || word.indexOf('!') === word.length-1 || word.indexOf('.') === word.length-1) delay += (delay *1.8);
+    if(word.length > 2) if(word.indexOf(';') === word.length-2 || word.indexOf('?') === word.length-2 || word.indexOf('!') === word.length-2 || word.indexOf('.') === word.length-2) delay += (delay *1.8);
+
+    var bit = -1;
+    while(word.length < 22){
+      if(bit > 0){
+          word = word + ' ';
+      }
+      else{
+          word = ' ' + word;
+      }
+      bit = bit * -1;
+    }
+
+
+    var start = '';
+    var end = '';
+    if((length % 2) === 0){
+      start = word.slice(0, word.length/2);
+      end = word.slice(word.length/2, word.length);
+    } else{
+      start = word.slice(0, word.length/2);
+      end = word.slice(word.length/2, word.length);
+    }
+
+
+    console.log(word);
+    $(element).html(start.slice(0, start.length -1) + "<span class='cursor'>" + start.slice(start.length-1, start.length) + "</span>"+ end);
+    //$(element).html("<span class='start'>" + start.slice(0, start.length -1) + "</span><span class='cursor'>" + start.slice(start.length-1, start.length) + "</span><span class='end'>"+ end + "</span>");
+
+    setTimeout(cb, delay);
+  };
+
+  async.eachSeries(text, showWord);
+
+
+
+};
